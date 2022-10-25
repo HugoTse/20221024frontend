d640c3f3ae0d9054a7ccd35c1c98362c1641fbda
HugoTse <hugotse8@gmail.com>
2022-10-24 19:12:02 -0700

    added login functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import '@aws-amplify/ui-react/styles.css';
 
 import {
   withAuthenticator,
+  ButtonGroup,
   Button,
   Heading,
   Image,
@@ -67,11 +68,236 @@ const theme: Theme = {
 };
 
 function App() {
+  const[logged, setLogged] = useState('');
+
+  // consts for creating account
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+  const[organizationid, setOrganizationid] = useState('');
+
+  // consts for logging in
+  const[loginusername, setLoginusername] = useState('');
+  const[loginpassword, setLoginpassword] = useState('');
+
+  // consts after logging in
+  const[loggedinusername, setLoggedinusername] = useState('');
+  const[loggedinaccounttype, setLoggedinaccounttype] = useState('');
+  const[loggedinorganizationid, setLoggedinorganizationid] = useState('');
+
+
+  // Function for logging out
+  async function logout(e){
+    e.preventDefault();
+    setLogged('');
+    console.log('Now logged out');
+  }
+
+  // Function for logging in
+  async function login(e){
+    e.preventDefault();
+    // Make sure username and password are filled in
+    if(loginusername != '' && loginpassword != ''){
+      console.log('The login username is: ' + loginusername);
+      console.log('The login password is: ' + loginpassword);
+      
+      // instantiate a headers object
+      var myHeaders = new Headers();
+      // add content type header to object
+      myHeaders.append("Content-Type", "application/json");
+      // Instantiate a request url
+      var url = "https://38nf63ncvl.execute-api.us-west-1.amazonaws.com/v0/login"+"?username="+loginusername+"&password="+loginpassword;
+      // create a JSON object with parameters for API call and store in a variable
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+      // Make the API call
+      const apiResponse = await fetch(url, requestOptions )
+      const apiResponseJSON = await apiResponse.json()
+      // console.log(apiResponseJSON);
+      // console.log(apiResponseJSON.Found);
+      if(apiResponseJSON.Found == 'False'){
+        alert('Please try logging again.')
+      }
+      if(apiResponseJSON.Found == 'True'){
+        setLogged(apiResponse.username)
+        setLoggedinusername(apiResponseJSON.username);
+        setLoggedinorganizationid(apiResponseJSON.organizationid);
+        setLoggedinaccounttype(apiResponseJSON.accounttype);
+        console.log('loggedinusername= ' + loggedinusername);
+        console.log('loggedinorganizationid= ' + loggedinorganizationid);
+        console.log('loggedinaccounttype= ' + loggedinaccounttype);
+      }
+      console.log('Now logged in');
+    }
+  }
+
+
+  // Function for creating an owner account
+  async function createOwneraccount(e){
+    if(username != '' && password != ''){
+      console.log('The username is ' + username);
+      console.log('The password is ' + password);
+      console.log('The organization ID is ' + organizationid);
+
+      // Instantiate a request url
+      var url = "https://38nf63ncvl.execute-api.us-west-1.amazonaws.com/v0/createaccount"+"?username="+username+"&password="+password+"&organizationid="+organizationid+"&accounttype=owner";
+      console.log(url);
+      // instantiate a headers object
+      var myHeaders = new Headers();
+      // add content type header to object
+      myHeaders.append("Content-Type", "application/json");
+      // create a JSON object with parameters for API call and store in a variable
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+      // make API call with parameters and use promises to get response
+      await fetch(url, requestOptions)
+        .then((response) => response.text())
+        // .then((result) => alert(JSON.parse(result).body))
+        .catch((error) => console.log("error", error));
+
+      setUsername('');
+      setPassword('');
+      setOrganizationid('');
+      alert("You have created an owner account");
+    }
+  }
+
+  // Function for creating an administrator account
+  async function createAdministratoraccount(e){
+    if(username != '' && password != '' && organizationid != ''){
+      console.log('The username is ' + username);
+      console.log('The password is ' + password);
+      console.log('The organization ID is ' + organizationid);
+
+      // Instantiate a request url
+      var url = "https://38nf63ncvl.execute-api.us-west-1.amazonaws.com/v0/createaccount"+"?username="+username+"&password="+password+"&organizationid="+organizationid+"&accounttype=administrator";
+      console.log(url);
+      // instantiate a headers object
+      var myHeaders = new Headers();
+      // add content type header to object
+      myHeaders.append("Content-Type", "application/json");
+      // create a JSON object with parameters for API call and store in a variable
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+      // make API call with parameters and use promises to get response
+      await fetch(url, requestOptions)
+        .then((response) => response.text())
+        // .then((result) => alert(JSON.parse(result).body))
+        .catch((error) => console.log("error", error));
+
+      setUsername('');
+      setPassword('');
+      setOrganizationid('');
+      alert("You have created a new administrator account");
+    }
+  }
+
+  // Function for creating a user account
+  async function createUseraccount(e){
+    if(username != '' && password != '' && organizationid != ''){
+      console.log('The username is ' + username);
+      console.log('The password is ' + password);
+      console.log('The organization ID is ' + organizationid);
+
+      // Instantiate a request url
+      var url = "https://38nf63ncvl.execute-api.us-west-1.amazonaws.com/v0/createaccount"+"?username="+username+"&password="+password+"&organizationid="+organizationid+"&accounttype=user";
+      console.log(url);
+      // instantiate a headers object
+      var myHeaders = new Headers();
+      // add content type header to object
+      myHeaders.append("Content-Type", "application/json");
+      // create a JSON object with parameters for API call and store in a variable
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+      // make API call with parameters and use promises to get response
+      await fetch(url, requestOptions)
+        .then((response) => response.text())
+        // .then((result) => alert(JSON.parse(result).body))
+        .catch((error) => console.log("error", error));
+
+      setUsername('');
+      setPassword('');
+      setOrganizationid('');
+      alert("You have created a new user account");
+    }
+  }
+
   return (
     <div className="App">
 
       <Heading level={1}>Ransomware Defender</Heading>
       <h4><i>"Using processes, people, and technology to keep your files safe"</i></h4>
+      
+      {logged == '' ? (<>
+        <Card variation="outlined">
+          <div className='login'>
+            <Tabs>
+              <TabItem title="Create Account">
+                <br/>
+                <TextField
+                  label="Username"
+                  onChange={e => setUsername(e.target.value)}
+                />
+                <br/>
+                <TextField
+                  label="Password"
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <br/>
+                <TextField
+                  label="Organization ID (for administrators and users)"
+                  onChange={e => setOrganizationid(e.target.value)}
+                />
+                <br/>
+                <p>Choose your type of account:</p>
+                <ButtonGroup variation="primary">
+                  <Button variation='primary'onClick={e =>createOwneraccount(e)}>Owner</Button>
+                  <Button variation='primary'onClick={e =>createAdministratoraccount(e)}>Administrator</Button>
+                  <Button variation='primary' onClick={e =>createUseraccount(e)}>User</Button>
+                </ButtonGroup>
+              </TabItem>
+              <TabItem title="Login">
+                <br/>
+                <TextField
+                  label="Username"
+                  onChange={e => setLoginusername(e.target.value)}
+                />
+                <br/>
+                <TextField
+                  label="Password"
+                  onChange={e => setLoginpassword(e.target.value)}
+                />
+                <br/>
+                <ButtonGroup variation="primary">
+                  <Button variation='primary'onClick={e =>login(e)}>LOG IN</Button>
+                </ButtonGroup>
+              </TabItem>
+            </Tabs>
+          </div>
+        </Card>
+        <br/>
+      </> ) :
+      (<>
+      <Card>
+        <Button variation='primary'
+        onClick={logout}
+        >LOG OUT</Button>
+      </Card>
+      </>) }
+
+     
+      
 
 
        <ThemeProvider theme={theme} colorMode="light">
